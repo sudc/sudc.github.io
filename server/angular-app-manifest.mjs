7c2f95310c88740030abb7803b64ce0dfaaf521f
sudc <sudarshan.choudhary5@gmail.com>
2025-12-10 20:59:29 +0000

    Deploying to gh-pages from @ sudc/sudc.github.io@47b67e173e597a2d2513d515eadae5068816a142 🚀

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,8 +12,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 841, hash: 'ca51b338bed2afae5c1f51c79b2987ac4acda3a5216713db59b20d206b342be8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 952, hash: 'cc151d28e243297b2bcae0fd3ba3e5be48ebdb9ecc5553ac978b931d9de0050f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 841, hash: 'ae0a84008f284da60dddb485fbb26181cad58bd724d6ddccc624d3cf52b43b81', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 952, hash: '814ed76ab5b0ec2dd1f4875c30586a89e6d1b94b7e26d70359b68ec095c20994', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'index.html': {size: 8110, hash: '10cd44a4286035b0346ed55bcfd8eae7cd61a70075f5796df7dbc2624c6d5501', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-ZT36SWQW.css': {size: 2868, hash: 'pc5HZjyL6tQ', text: () => import('./assets-chunks/styles-ZT36SWQW_css.mjs').then(m => m.default)}
   },
